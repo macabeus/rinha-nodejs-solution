4303ee3adcf889bc9c5e95a4bb32d0474190d049
macabeus <bruno.macabeus@gmail.com>
2024-02-16 01:30:31 +0000

    update

--- script.sql
+++ script.sql
@@ -1,16 +1,63 @@
-CREATE TABLE clientes (
+CREATE TYPE transacao_tipo AS ENUM ('c', 'd');
+
+CREATE TABLE IF NOT EXISTS clientes (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
-  limite INTEGER NOT NULL
+  limite INTEGER NOT NULL,
+  saldo INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS transacoes (
+  id SERIAL PRIMARY KEY,
+  cliente_id integer REFERENCES clientes,
+  valor INTEGER NOT NULL,
+  tipo transacao_tipo NOT NULL,
+  descricao TEXT NOT NULL,
+  realizada_em TIMESTAMP NOT NULL
+);
+
+CREATE PROCEDURE InserirTransacao(
+  _cliente_id integer,
+  _valor integer,
+  _tipo transacao_tipo,
+  _descricao text,
+  OUT saldo_atual integer
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    cliente_saldo INTEGER;
+    cliente_limite INTEGER;
+    novo_saldo INTEGER;
+BEGIN
+  SELECT saldo, limite INTO cliente_saldo, cliente_limite FROM clientes WHERE id = _cliente_id;
+  
+  IF _tipo = 'd' THEN
+    IF (cliente_saldo - _valor) < cliente_limite THEN
+      RAISE EXCEPTION 'Saldo insuficiente para realizar a transacao';
+    END IF;
+
+    novo_saldo := cliente_saldo - _valor;
+    UPDATE clientes SET saldo = novo_saldo WHERE id = _cliente_id RETURNING saldo INTO saldo_atual;
+  ELSE
+    novo_saldo := cliente_saldo + _valor;
+    UPDATE clientes SET saldo = novo_saldo WHERE id = _cliente_id RETURNING saldo INTO saldo_atual;
+  END IF;
+
+  INSERT INTO transacoes(cliente_id, valor, tipo, descricao, realizada_em)
+  VALUES(_cliente_id, _valor, _tipo, _descricao, NOW());
+END;
+$$;
+
+-- seeds
 
 DO $$
 BEGIN
-  INSERT INTO clientes (nome, limite)
+  INSERT INTO clientes (nome, limite, saldo)
   VALUES
-    ('o barato sai caro', 1000 * 100),
-    ('zan corp ltda', 800 * 100),
-    ('les cruders', 10000 * 100),
-    ('padaria joia de cocaia', 100000 * 100),
-    ('kid mais', 5000 * 100);
+    ('o barato sai caro', 1000 * 100, 0),
+    ('zan corp ltda', 800 * 100, 0),
+    ('les cruders', 10000 * 100, 0),
+    ('padaria joia de cocaia', 100000 * 100, 0),
+    ('kid mais', 5000 * 100, 0);
 END; $$
--- src/main.js
+++ src/main.js
@@ -1,8 +1,70 @@
+const { Client } = require('pg');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const client = new Client({
+  host: 'db',
+  port: 5432,
+  user: 'admin',
+  password: '123',
+  database: 'rinha',
 })
+
+const databaseSetup = async () => {
+  await client.connect()
+}
+
+databaseSetup()
+
+app.use(express.json())
+
+app.get('/show_clientes', async (req, res) => {
+  const queryResult = await client.query(`SELECT * FROM clientes`)
+  res.send(JSON.stringify(queryResult.rows))
+})
+
+app.get('/show_transacoes', async (req, res) => {
+  const queryResult = await client.query(`SELECT * FROM transacoes`)
+  res.send(JSON.stringify(queryResult.rows))
+})
+
+app.get('/clientes/:id/extrato', async (req, res) => {
+  const queryResult = await client.query(`SELECT nome, limite FROM clientes WHERE id = ${req.params.id}`)
+  res.send(`Result: ${queryResult.rows[0].limite}`)
+})
+
+app.post('/clientes/:id/transacoes', async (req, res) => {
+  // INSERT INTO lists VALUES ((SELECT max(id)+1 FROM lists),'KO','SPH', '5');
+
+  const {
+    valor,
+    tipo,
+    descricao,
+  } = req.body
+
+  const queryResult = await client.query(`
+    DO $$
+    DECLARE
+        novo_saldo integer;
+    BEGIN
+        CALL InserirTransacao(${req.params.id}, ${valor}, '${tipo}'::transacao_tipo, '${descricao}', novo_saldo);
+
+        SELECT novo_saldo INTO novo_saldo;
+    END;
+    $$;
+  `)
+
+  res.send(JSON.stringify(queryResult.rows))
+
+  // res.send(`{ "limite": ${queryResult.rows[0].limite}, "saldo": ${queryResult.rows[0].saldo} }`)
+})
+
+// POST /clientes/[id]/transacoes
+
+// {
+//     "valor": 1000,
+//     "tipo" : "c",
+//     "descricao" : "descricao"
+// }
 
 app.listen(8080, () => console.log('Server is up and running'));
